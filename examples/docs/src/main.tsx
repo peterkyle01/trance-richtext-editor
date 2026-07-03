5135d5b067a17829fc2554401182daf85d7a330e
peterkyle01 <kylepeterkoine4@gmail.com>
2026-07-03 08:33:16 +0300

    Add @vercel/analytics to docs example app

--- examples/docs/src/main.tsx
+++ examples/docs/src/main.tsx
@@ -2,10 +2,12 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import 'trance-richtext-editor/styles.css';
+import { Analytics } from "@vercel/analytics/next"
 import './App.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <Analytics />
   </StrictMode>,
 );
